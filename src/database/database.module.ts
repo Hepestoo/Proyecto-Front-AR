ff19f07b61b9a6d958408eb88105176f1637e2b1
Hepestoo <teckalan2@gmail.com>
2024-11-20 23:40:57 -0500

    clase 10

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,8 +1,29 @@
 import { Module } from '@nestjs/common';
 import { databaseProvider } from './database.provides';
 import { ConfigService } from 'src/config/config.service';
+import { TypeORMError } from 'typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from 'src/config/config.module';
+import { config } from 'dotenv';
 
 @Module({
+    imports:[
+        TypeOrmModule.forRootAsync({
+            imports:[ConfigModule],
+            inject:[ConfigModule],
+            useFactory:(config: ConfigService)=>({
+                type:'postgres',
+                host:config.get('HOST') || 'localhost',
+                port: +config.get('PORT_DB'),
+                username: config.get('USERNAME')||'root',
+                password: config.get('PASSWORD')||'prueba',
+                database: config.get('DATABASE'),
+                entities:[
+                    __dirname + '/../**/*.entity{.ts,.js}',
+                ]
+            })
+        })
+    ],
     providers:[...databaseProvider, ConfigService],
     exports:[...databaseProvider]
 })
